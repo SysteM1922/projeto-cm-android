14b4d8305c839b17f7fc8f5cca7cfd1affcdac38
Gui <guicostaantunes@gmail.com>
2024-12-23 21:11:24 +0000

    add nfc card image

--- cm-android/app/src/main/java/com/example/cmprojectandroid/screens/NFCPage.kt
+++ cm-android/app/src/main/java/com/example/cmprojectandroid/screens/NFCPage.kt
@@ -1,20 +1,62 @@
 package com.example.cmprojectandroid.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cmprojectandroid.R
 
 @Composable
 fun NFCPage() {
-    Text(
-        text = "NFC Page",
-        style = MaterialTheme.typography.headlineMedium,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Please tap your card on the NFC reader.",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            textAlign = TextAlign.Center
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.card),
+                contentDescription = "NFC Card",
+                modifier = Modifier
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = MaterialTheme.shapes.extraLarge,
+                    ),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NFCPagePreview() {
+    NFCPage()
 }
